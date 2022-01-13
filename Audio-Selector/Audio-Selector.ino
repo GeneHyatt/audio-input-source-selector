8d05e6d84c505f466d14ae70a6f250fe3fa17123
Gene Hyatt <gene.hyatt@gmail.com>
2022-01-13 18:06:18 -0500

    Added some constants that may not be needed. Establish two digital pins and put them in the loop.

--- Audio-Selector/Audio-Selector.ino
+++ Audio-Selector/Audio-Selector.ino
@@ -10,6 +10,9 @@
 #define TFT_RST 9 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC 8
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DEVICEPIN_A 4     // Digital pin connected to turntable
+#define DEVICEPIN_B 5     // Digital pin connected to tape
+
 #define DHTTYPE DHT11
 #define ENC_BUTT_PIN 24
 #define NEO_PIN 6
@@ -21,10 +24,24 @@ seesaw_NeoPixel pixel = seesaw_NeoPixel(1, NEO_PIN, NEO_GRB + NEO_KHZ800);
 // For 1.44" and 1.8" TFT with ST7735 use:
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
+// color definitions
+const uint16_t  Display_Color_Black        = 0x0000;
+const uint16_t  Display_Color_Blue         = 0x001F;
+const uint16_t  Display_Color_Red          = 0xF800;
+const uint16_t  Display_Color_Green        = 0x07E0;
+const uint16_t  Display_Color_Cyan         = 0x07FF;
+const uint16_t  Display_Color_Magenta      = 0xF81F;
+const uint16_t  Display_Color_Yellow       = 0xFFE0;
+const uint16_t  Display_Color_White        = 0xFFFF;
+
 // Initialize DHT sensor.
 DHT dht(DHTPIN, DHTTYPE);
 int32_t encPos;
+uint16_t        Display_Text_Color         = Display_Color_Green;
+uint16_t        Display_Background_Color    = Display_Color_Black;
 bool dhtEnabled = false;
+volatile bool screenState = true;
+bool            isDisplayVisible        = false;
 
 void setup(void) {
   Serial.begin(115200);
@@ -34,10 +51,15 @@ void setup(void) {
     Serial.println("Couldn't find seesaw on default address");
     while (1) delay(10);
   }
+
+  pinMode(DEVICEPIN_A, OUTPUT);
+  pinMode(DEVICEPIN_B, OUTPUT);
+
+  screenState = false;
   Serial.println("seesaw started");
 
   Serial.println("Setup Pixel");
-  pixel.setBrightness(50);
+  pixel.setBrightness(30);
   pixel.show();
 
   // use a pin for the built in encoder switch
@@ -50,8 +72,8 @@ void setup(void) {
   delay(10);
   ss.setGPIOInterrupts((uint32_t)1 << ENC_BUTT_PIN, 1);
   ss.enableEncoderInterrupt();
-  Serial.println("Turning on DHT sensor");
 
+  Serial.println("Turning on DHT sensor");
   dht.begin();
   delay(10);
   if (!dht.readTemperature()) {
@@ -62,21 +84,27 @@ void setup(void) {
 
   Serial.println("Turning on Display");
   tft.initR(INITR_BLACKTAB);
+  tft.setFont();
+  tft.fillScreen(Display_Background_Color);
+  tft.setTextColor(Display_Text_Color, Display_Background_Color);
+  tft.setTextSize(1);
   tft.setRotation(1);
   tft.setTextWrap(true);
+  // the display is now on
+  isDisplayVisible = true;
   Serial.println("Startup complete.");
   Serial.println("Enjoy!!");
 }
 
 void loop() {
 
+  tft.enableDisplay(isDisplayVisible);
+
   if (! ss.digitalRead(ENC_BUTT_PIN)) {
-    Serial.println("Button pressed!");
-    tftPrintRunTime();
-    if (dhtEnabled) {
-      drawAmbientData();
-    }
+    buttonPress();
   }
+
+
 
   int32_t newPos = ss.getEncoderPosition();
   // did we move around?
@@ -85,6 +113,23 @@ void loop() {
     tft.setCursor(0, 100);
     tft.print(F("Input select = "));
     tft.print(newPos);
+
+    if (newPos >= 1 && newPos <= 10) {
+      digitalWrite(DEVICEPIN_A, HIGH);
+      digitalWrite(DEVICEPIN_B, LOW);
+      Serial.println(digitalRead(DEVICEPIN_A));
+      Serial.println(digitalRead(DEVICEPIN_B));
+    } else if (newPos >= 11 && newPos < 21) {
+      digitalWrite(DEVICEPIN_A, LOW);
+      digitalWrite(DEVICEPIN_B, HIGH);
+      Serial.println(digitalRead(DEVICEPIN_A));
+      Serial.println(digitalRead(DEVICEPIN_B));
+    } else {
+      digitalWrite(DEVICEPIN_A, LOW);
+      digitalWrite(DEVICEPIN_B, LOW);
+      Serial.println(digitalRead(DEVICEPIN_A));
+      Serial.println(digitalRead(DEVICEPIN_B));
+    }
 
     // change the neopixel color
     pixel.setPixelColor(0, Wheel(newPos & 0xFF));
@@ -111,6 +156,27 @@ uint32_t Wheel(byte wPOS) {
   return pixel.Color(wPOS * 3, 255 - wPOS * 3, 0);
 }
 
+
+void buttonPress() {
+  screenState = !screenState;
+  Serial.print("Button pressed with interupt ");
+  Serial.println(screenState);
+  if (screenState) {
+    drawCurrentDevice();
+  } else {
+    tftPrintRunTime();
+    if (dhtEnabled) {
+      drawAmbientData();
+    }
+  }
+}
+
+void drawCurrentDevice() {
+  tft.setTextColor(Display_Text_Color, Display_Background_Color);
+  tft.setCursor(0, 0);
+  tft.println("Turntable");
+}
+
 void drawAmbientData() {
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -119,9 +185,9 @@ void drawAmbientData() {
   float f = dht.readTemperature(true);
   // Compute heat index in Fahrenheit
   float hif = dht.computeHeatIndex(f, h);
-  delay(1500);
-  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
-  tft.setCursor(0, 0);
+
+  tft.setTextColor(Display_Text_Color, Display_Background_Color);
+  tft.setCursor(0, 50);;
   tft.print(F("Temp: "));
   tft.print(f);
   tft.print((char)247);
